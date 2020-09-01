4aeb4f45efd0f60ef009a7ba7cee2ff49de76256
Vacumsio <vacumsio@gmail.com>
2020-09-01 17:41:27 +0300

    редирект api

---
+++ AbilityTestWebAPI/Controllers/LaunchWeatherController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbilityTestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaunchWeatherController : ControllerBase
+    {
+
+        public IActionResult SecondaryLaunchWeather()
+        {
+            return RedirectToAction("Get", "WeatherForecast");
+        }
+    }
+}
---
+++ AbilityTestWebAPI/Controllers/ThirdPartyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbilityTestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThirdPartyController : ControllerBase
+    {
+        public IActionResult ThirdPartyWeather()
+        {
+            return RedirectToAction("SecondaryLaunchWeather", "LaunchWeather");
+        }
+    }
+}
